e5177528579541aeb7c733fbc8d3e4426f6ae928
Karel Kočí <cynerd@email.cz>
2023-10-04 19:43:54 +0200

    Complete ls and dir arguments as paths

--- shvcli/complet.py
+++ shvcli/complet.py
@@ -5,7 +5,6 @@ import typing
 
 from prompt_toolkit.completion import CompleteEvent, Completer, Completion
 from prompt_toolkit.document import Document
-from shv import RpcError
 
 from .client import SHVClient
 from .config import CliConfig
@@ -40,12 +39,6 @@ class CliCompleter(Completer):
         self.shvclient = shvclient
         self.config = config
 
-    def _comppath(self, items: CliItems) -> tuple[pathlib.PurePosixPath, str]:
-        onpath = (items.path if items.path else items.method).rsplit("/", maxsplit=1)
-        if len(onpath) == 1:
-            return self.config.path, onpath[0]
-        return self.config.path / onpath[0], onpath[1]
-
     def get_completions(
         self, document: Document, complete_event: CompleteEvent
     ) -> typing.Iterable[Completion]:
@@ -56,21 +49,13 @@ class CliCompleter(Completer):
         if CliFlags.COMPLETE_CALL in items.flags:
             if (desc := self.INTERNAL.get(items.method, None)) is not None:
                 yield from _comp_from(items.param_raw, desc)
+            if items.method in ("ls", "dir") and not items.param_raw.startswith('"'):
+                yield from self._complete_paths(items)
             return  # Nothing to complete because we can't complete CPON
 
         # Paths
         if CliFlags.HAS_COLON not in items.flags:
-            pth, comp = self._comppath(items)
-            node = self.shvclient.tree.get_path(pth)
-            if node is not None:
-                if comp in node:
-                    yield Completion(f"{comp}:", start_position=-len(comp))
-                    yield from (
-                        Completion(f"{comp}/{n}", start_position=-len(comp))
-                        for n in node[comp]
-                    )
-                else:
-                    yield from _comp_from(comp, node)
+            yield from self._complete_paths(items)
             if items.path:
                 return  # Completing only path now so do not follow with methods
 
@@ -84,12 +69,43 @@ class CliCompleter(Completer):
             ),
         )
 
+    def _comppath(self, items: CliItems) -> tuple[pathlib.PurePosixPath, str]:
+        if CliFlags.COMPLETE_CALL in items.flags:
+            dynpath = items.param_raw
+            basepath = self.config.path / items.path
+        elif items.path:
+            dynpath = items.path
+            basepath = self.config.path
+        else:
+            dynpath = items.method
+            basepath = self.config.path
+        onpath = dynpath.rsplit("/", maxsplit=1)
+        if len(onpath) == 1:
+            return basepath, onpath[0]
+        return basepath / onpath[0], onpath[1]
+
+    def _complete_paths(self, items: CliItems) -> typing.Iterable[Completion]:
+        pth, comp = self._comppath(items)
+        node = self.shvclient.tree.get_path(pth)
+        if node is not None:
+            if comp in node:
+                yield Completion(f"{comp}:", start_position=-len(comp))
+                yield from (
+                    Completion(f"{comp}/{n}", start_position=-len(comp))
+                    for n in node[comp]
+                )
+            else:
+                yield from _comp_from(comp, node)
+
     async def get_completions_async(
         self, document: Document, complete_event: CompleteEvent
     ) -> typing.AsyncGenerator[Completion, None]:
         """Completions as async generator."""
         items = parse_line(document.text)
-        if self.config.autoprobe and CliFlags.COMPLETE_CALL not in items.flags:
+        if self.config.autoprobe and (
+            CliFlags.COMPLETE_CALL not in items.flags
+            or (items.method in ("ls", "dir") and not items.param_raw.startswith('"'))
+        ):
             if CliFlags.HAS_COLON in items.flags:
                 await self.shvclient.probe(self.config.shvpath(items.path))
             else:
