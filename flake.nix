1e0f3dbf1b5f9f4db65e9f2940cd0f3a793797fd
Karel Kočí <kkoci@elektroline.cz>
2025-04-05 20:41:34 +0200

    flake.nix: fix with latest nixpkgs change
    This also moves the inputs to track pyshv inputs. This simplifies the
    inclusion tree and shvcli is part of the pyshv project thus it makes
    sense to keep these projects in sync.

--- flake.nix
+++ flake.nix
@@ -4,90 +4,66 @@
 
   outputs = {
     self,
-    flake-utils,
-    nixpkgs,
     pyshv,
   }: let
-    inherit (builtins) match;
-    inherit (flake-utils.lib) eachSystem defaultSystems filterPackages;
-    inherit (nixpkgs.lib) head trivial hasSuffix attrValues getAttrs composeManyExtensions;
+    inherit (pyshv.inputs) nixpkgs flakepy;
+    inherit (flakepy.inputs.flake-utils.lib) eachSystem defaultSystems;
+    inherit (nixpkgs.lib) composeManyExtensions;
 
-    pyproject = trivial.importTOML ./pyproject.toml;
-    inherit (pyproject.project) name version;
-    src = builtins.path {
-      path = ./.;
-      filter = path: _: ! hasSuffix ".nix" path;
-    };
-
-    pypi2nix = list: pypkgs:
-      attrValues (getAttrs (map (n: let
-          pyname = head (match "([^ =<>;]*).*" n);
-          pymap = {};
-        in
-          pymap."${pyname}" or pyname)
-        list)
-        pypkgs);
-    requires = pypi2nix pyproject.project.dependencies;
+    pyproject = flakepy.lib.pyproject ./. {};
 
     pypackage = {
-      buildPythonPackage,
-      pythonPackages,
       python,
-      setuptools,
       runCommandLocal,
     }:
-      buildPythonPackage {
-        pname = pyproject.project.name;
-        inherit version src;
-        pyproject = true;
-        build-system = [setuptools];
-        propagatedBuildInputs = requires pythonPackages;
+      pyproject.buildPackage python {
         pythonImportsCheck = ["shvcli"];
         meta.mainProgram = "shvcli";
 
         passthru.withPlugins = plugins:
-          runCommandLocal "shvcli-${version}" {
-            env = python.buildEnv.override {extraLibs = [pythonPackages.shvcli] ++ plugins;};
+          runCommandLocal "shvcli-${pyproject.version}" {
+            env = python.buildEnv.override {extraLibs = [python.pkgs.shvcli] ++ plugins;};
           } "mkdir -p $out/bin && ln -sf $env/bin/shvcli $out/bin/shvcli";
       };
   in
     {
       overlays = {
-        pythonPackagesExtension = final: _: {
-          "${name}" = final.callPackage pypackage {};
+        pythonPackages = final: _: {
+          "${pyproject.pname}" = final.callPackage pypackage {};
         };
-        noInherit = final: prev: {
-          pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [self.overlays.pythonPackagesExtension];
-          "${name}" = final.python3Packages.toPythonApplication final.python3Packages.shvcli;
+        packages = final: prev: {
+          pythonPackagesExtensions =
+            prev.pythonPackagesExtensions ++ [self.overlays.pythonPackages];
+          "${pyproject.pname}" =
+            final.python3Packages.toPythonApplication final.python3Packages.shvcli;
         };
         default = composeManyExtensions [
           pyshv.overlays.default
-          self.overlays.noInherit
+          self.overlays.packages
         ];
       };
+      inherit self;
     }
     // eachSystem (defaultSystems ++ ["armv7l-linux"]) (system: let
       pkgs = nixpkgs.legacyPackages.${system}.extend self.overlays.default;
     in {
-      packages.default = pkgs."${name}";
+      packages.default = pkgs."${pyproject.pname}";
       legacyPackages = pkgs;
 
-      devShells = filterPackages system {
-        default = pkgs.mkShell {
-          packages = with pkgs; [
-            deadnix
-            editorconfig-checker
-            gitlint
-            ruff
-            shellcheck
-            shfmt
-            statix
-            (
-              python3.withPackages (p:
-                [p.build p.twine p.mypy] ++ (requires p))
-            )
-          ];
-        };
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          deadnix
+          editorconfig-checker
+          gitlint
+          mypy
+          ruff
+          shellcheck
+          shfmt
+          statix
+          twine
+          (python3.withPackages (pypkgs: with pypkgs; [build sphinx-autobuild]))
+        ];
+        inputsFrom = [self.packages.${system}.default];
       };
 
       checks.default = self.packages.${system}.default;
