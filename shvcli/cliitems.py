silicon-heaven/shvcli

commit 7bd6e14544d61913b3b8070883537e46dcb26f51

path: use path utility from upstream
The upstream pyshv now contains this code so it can be removed from
shvcli itself.

shvcli/cliitems.py
@@ -3,11 +3,9 @@
 import contextlib
 import string
 
-from shv import SHVType
+from shv import SHVPath, SHVType
 from shv.cpon import Cpon
 from shv.rpctypes import RpcType, RpcTypeParseError, rpctype_parse
-
-from .path import SHVPath
 
 
 class CliItems:

flake.nix
@@ -10,9 +10,9 @@
     inherit (flakepy.inputs.flake-utils.lib) eachSystem defaultSystems;
     inherit (nixpkgs.lib) composeManyExtensions;
 
-    pyproject = flakepy.lib.pyproject ./. {};
+    pyproject = flakepy.lib.readPyproject ./. {};
 
-    pypackage = pyproject.package ({
+    pypackage = pyproject.buildPackage ({
       runCommandLocal,
       python,
       shvcli,

shvcli/afterconn.py
@@ -5,9 +5,8 @@ import collections.abc
 import typing
 
 from .args import ArgsParseFuncGenT, register_argparser
-from .client import Client
+from .client import Client, SHVPath
 from .cliitems import CliItems
-from .path import SHVPath
 from .scan import scan_nodes
 from .state import StateVar
 
@@ -51,6 +50,6 @@ def _argparser(parser: argparse.ArgumentParser) -> ArgsParseFuncGenT:
         for ri in args.subscribe:
             await client.subscribe(CliItems.extend_ri(ri))
         if args.scan:
-            await scan_nodes(client, SHVPath(), args.scan_dept)
+            await scan_nodes(client, SHVPath(""), args.scan_dept)
 
     Afterconn(state).append(afterconn)

shvcli/client.py
@@ -10,12 +10,12 @@ from shv import (
     RpcMessage,
     RpcMethodNotFoundError,
     SHVClient,
+    SHVPath,
     SHVType,
 )
 
 from . import VERSION
 from .options import CallQueryTimeout, CallRetryTimeout
-from .path import SHVPath
 from .state import State
 from .tools.print import print_cpon
 from .tree import Node, Tree
@@ -38,7 +38,7 @@ class Client(SHVClient):
             Tree(self.state).valid_path(msg.path).methods.setdefault(msg.source, None)
             print_cpon(msg.param, f"{msg.path}:{msg.source}:{msg.signal_name}: ", True)
 
-    async def ls(self, path: str) -> list[str]:
+    async def ls(self, path: str | SHVPath) -> list[str]:
         """List same as in ValueClient but with result being preserved in tree."""
         res: list[str]
         try:
@@ -53,7 +53,7 @@ class Client(SHVClient):
 
         return res
 
-    async def dir(self, path: str, details: bool = False) -> list[RpcDir]:
+    async def dir(self, path: str | SHVPath, details: bool = False) -> list[RpcDir]:
         """List methods same as in ValueClient but result is being preserved in tree."""
         res: list[RpcDir]
         try:
@@ -70,7 +70,7 @@ class Client(SHVClient):
 
     async def call(
         self,
-        path: str,
+        path: str | SHVPath,
         method: str,
         *args: typing.Any,  # noqa ANN401
         **kwargs: typing.Any,  # noqa ANN401

shvcli/file.py
@@ -10,13 +10,12 @@ import shv
 from prompt_toolkit.shortcuts import ProgressBar, ProgressBarCounter
 
 from .client import Client
-from .path import SHVPath
 
 
 async def copy_file(
     client: Client,
-    src: pathlib.Path | SHVPath,
-    dest: pathlib.Path | SHVPath,
+    src: pathlib.Path | shv.SHVPath,
+    dest: pathlib.Path | shv.SHVPath,
     label: str = "",
 ) -> None:
     """Copy file with support for local and SHV RPC files.
@@ -28,12 +27,12 @@ async def copy_file(
     destfile: shv.RpcFile | io.IOBase | None = None
 
     try:
-        if isinstance(src, SHVPath):
+        if isinstance(src, shv.SHVPath):
             srcfile = shv.RpcFile(client, str(src), flags=shv.RpcFile.Flag(0))
             if not await srcfile.readable():
                 raise FileNotFoundError(f"No such readable SHV RPC file: {src}")
             srcsiz = await srcfile.size()
-        if isinstance(dest, SHVPath):
+        if isinstance(dest, shv.SHVPath):
             destfile = shv.RpcFile(client, str(dest), flags=shv.RpcFile.Flag(0))
             if not await destfile.writable():
                 raise FileNotFoundError(f"No such writable SHV RPC file: {dest}")

shvcli/path.py
@@ -1,229 +1,0 @@
-"""The extension for the PurePosixPath to support our root."""
-
-from __future__ import annotations
-
-import collections.abc
-import itertools
-import os
-import string
-import typing
-
-from shv.rpcri import shvpath_match
-
-
-class SHVPath:
-    """The SHV path helper.
-
-    This is designed to be close to the :mod:`pathlib` while providing the SHV
-    path specific behavior. But some of the methods just doesn't make sense in
-    the respect of the SHV path (such as ``is_absolute``).
-
-    We can't easilly use :mod:`pathlib` based version because there are huge
-    differences in the implementation (not external API) between Python 3.11
-    and 3.12 and later versions.
-
-    The rules for the SHV path are:
-
-    - White characters (like spaces) are not allowed.
-    - Path can't be absolute (can't start with ``/``).
-    - ``""`` is parent of every path.
-    - The combination of SHV and absolute path (appending absolute path to SHV)
-      results into only absolute path being used (while leading ``/`` is removed).
-      This behavior is not derived from SHV path but rather from :mod:`pathlib`.
-    - ``..`` is being interpreted as upped directory and thus removes upper
-      node from the path. This is outside of the standard SHV path behavior as
-      it makes it impossible for path to contain node ``..``.
-    """
-
-    def __init__(self, *pathsegments: str | os.PathLike) -> None:
-        parts: list[str] = []
-        for segment in itertools.chain.from_iterable(
-            os.fspath(pth).split("/") for pth in pathsegments if pth
-        ):
-            if any(c in segment for c in string.whitespace):
-                raise ValueError("SHV path can't contain white space characters")
-            if segment == "..":
-                if parts:
-                    parts.pop()
-            elif segment:
-                parts.append(segment)
-            else:
-                parts = []
-        self._parts = tuple(parts)
-
-        self._parents: SHVPathParents | None = None
-        self._hash: int | None = None
-
-    @property
-    def parts(self) -> tuple[str, ...]:
-        """Tuple giving access to the path's various components."""
-        return self._parts
-
-    def __str__(self) -> str:
-        return "/".join(self._parts)
-
-    def __fspath__(self) -> str:
-        return str(self)
-
-    def __repr__(self) -> str:
-        return f"{self.__class__.__name__}({str(self)!r})"
-
-    def __reduce__(self) -> tuple[type[SHVPath], tuple[str, ...]]:
-        return self.__class__, tuple(self._parts)  # pragma: no coverage
-
-    def __bool__(self) -> bool:
-        return bool(self._parts)
-
-    def __truediv__(self, key: str | os.PathLike | SHVPath) -> SHVPath:
-        return type(self)(*self._parts, key)
-
-    def __rtruediv__(self, key: str | os.PathLike) -> SHVPath:
-        return type(self)(key, *self._parts)
-
-    def __hash__(self) -> int:
-        if self._hash is None:
-            self._hash = hash(tuple(self._parts))
-        return self._hash
-
-    def __eq__(self, other: object) -> bool:
-        if isinstance(other, str | os.PathLike):
-            return os.fspath(self) == os.fspath(other)
-        return super().__eq__(other)
-
-    def __lt__(self, other: object) -> bool:
-        if isinstance(other, str | os.PathLike):
-            return os.fspath(self) < os.fspath(other)
-        raise TypeError(f"unsupported '<' between '{type(self)}' and '{type(other)}'")
-
-    def __le__(self, other: object) -> bool:
-        if isinstance(other, str | os.PathLike):
-            return os.fspath(self) <= os.fspath(other)
-        raise TypeError(f"unsupported '<=' between '{type(self)}' and '{type(other)}'")
-
-    def __gt__(self, other: object) -> bool:
-        if isinstance(other, str | os.PathLike):
-            return os.fspath(self) > os.fspath(other)
-        raise TypeError(f"unsupported '>' between '{type(self)}' and '{type(other)}'")
-
-    def __ge__(self, other: object) -> bool:
-        if isinstance(other, str | os.PathLike):
-            return os.fspath(self) >= os.fspath(other)
-        raise TypeError(f"unsupported '>=' between '{type(self)}' and '{type(other)}'")
-
-    @property
-    def parents(self) -> SHVPathParents:
-        """An immutable sequence providing access to the logical ancestors of the path."""
-        if self._parents is None:
-            self._parents = SHVPathParents(self)
-        return self._parents
-
-    @property
-    def parent(self) -> SHVPath:
-        """The logival parent of the path."""
-        if self._parts:
-            return type(self)(*self._parts[:-1])
-        return self
-
-    @property
-    def name(self) -> str:
-        """A string representing the final path component, if any."""
-        return self._parts[-1] if self._parts else ""
-
-    @property
-    def suffix(self) -> str:
-        """The last dot-separated portion of the final component, if any."""
-        _, dot, suffix = self.name.rpartition(".")
-        return f"{dot}{suffix}"
-
-    @property
-    def suffixes(self) -> list[str]:
-        """The last dot-separated portion of the final component, if any."""
-        return list(f".{s}" for s in self.name.split(".")[1:])
-
-    @property
-    def stem(self) -> str:
-        """The final path component, without its suffix."""
-        return self.name.partition(".")[0]
-
-    def is_relative_to(self, path: str | os.PathLike | SHVPath) -> bool:
-        """Return whether or not this path is relative to the other path."""
-        if not isinstance(path, SHVPath):
-            path = SHVPath(path)
-        print(path.parts)
-        return path._parts == self._parts[: len(path._parts)]
-
-    def joinpath(self, *pathsegments: str | os.PathLike) -> SHVPath:
-        """Join paths toggether.
-
-        Calling this method is equivalent to combining the path with each of
-        the given pathsegments in turn.
-        """
-        return type(self)(self, *pathsegments)
-
-    def full_match(self, pattern: str) -> bool:
-        """Match this path against the provided glob-style pattern."""
-        return shvpath_match(pattern, str(self))
-
-    def match(self, pattern: str) -> bool:
-        """Match this path against the provided glob-style pattern."""
-        return any(
-            shvpath_match(pattern, "/".join(parts))
-            for parts in map(lambda i: self._parts[i:], range(len(self._parts)))
-        )
-
-    def relative_to(self, other: str | os.PathLike | SHVPath) -> SHVPath:
-        """Compute a version of this path relative to the path represented by other."""
-        if not isinstance(other, SHVPath):
-            other = SHVPath(other)
-        if self._parts[: len(other._parts)] != other._parts:
-            raise ValueError(f"{self} is not relative to {other}")
-        return SHVPath(*self._parts[len(other._parts) :])
-
-    def with_name(self, name: str) -> SHVPath:
-        """Return a new path with the name changed."""
-        if not self._parts:
-            raise ValueError("Root path has an empty name.")
-        return type(self)(*self._parts[:-1], name)
-
-    def with_stem(self, stem: str) -> SHVPath:
-        """Return a new path with the stem changed."""
-        if not self._parts:
-            raise ValueError("Root path has an empty name.")
-        _, dot, suffix = self._parts[-1].partition(".")
-        return type(self)(*self._parts[:-1], f"{stem}{dot}{suffix}")
-
-    def with_suffix(self, suffix: str) -> SHVPath:
-        """Return a new path with the suffix changed."""
-        if not self._parts:
-            raise ValueError("Root path has an empty name.")
-        stem, _, _ = self._parts[-1].partition(".")
-        return type(self)(*self._parts[:-1], f"{stem}{suffix}")
-
-
-class SHVPathParents(collections.abc.Sequence[SHVPath]):
-    """The parents generator for :class:`SHVPath`."""
-
-    def __init__(self, path: SHVPath) -> None:
-        self._path = path
-
-    @typing.overload
-    def __getitem__(self, i: int) -> SHVPath: ...
-
-    @typing.overload
-    def __getitem__(
-        self, i: slice[typing.Any, typing.Any, typing.Any]
-    ) -> collections.abc.Sequence[SHVPath]: ...
-
-    def __getitem__(
-        self, i: int | slice[typing.Any, typing.Any, typing.Any]
-    ) -> collections.abc.Sequence[SHVPath] | SHVPath:
-        parts = self._path.parts
-        if isinstance(i, slice):
-            return tuple(self[y] for y in range(*i.indices(len(parts))))
-
-        if -len(parts) <= i < len(parts):
-            return SHVPath(*parts[: -i - 1])
-        raise IndexError("index out of range for SHV path parent")
-
-    def __len__(self) -> int:
-        return len(self._path.parts)

shvcli/scan.py
@@ -6,8 +6,7 @@ directly when configured.
 
 from prompt_toolkit.shortcuts import ProgressBar, ProgressBarCounter
 
-from .client import Client
-from .path import SHVPath
+from .client import Client, SHVPath
 
 
 async def scan_nodes(client: Client, path: SHVPath, depth: int = 3) -> None:

shvcli/state.py
@@ -11,8 +11,6 @@ import typing
 
 import shv
 
-from .path import SHVPath
-
 
 class StateVarMeta(abc.ABCMeta):
     """The Meta class for the StateVar."""
@@ -69,7 +67,7 @@ class State(dict[type[StateVar], StateVar]):
 
     def __init__(self) -> None:
         super().__init__()
-        self.path: SHVPath = SHVPath()
+        self.path: shv.SHVPath = shv.SHVPath()
         """Current path we are working relative to."""
 
         for cls in StateVar.variables:

commit 91eb8f87d2f8f1ba9086815ce87d7770281d2098

cliitems: fix path prefix change
The issue here was in combination of / at the end being interpreted as
root.

This regression was most likely caused by
7bd6e14544d61913b3b8070883537e46dcb26f51.

shvcli/cliitems.py
@@ -29,7 +29,9 @@ class CliItems:
         path = "" if ":" not in ri and "/" not in ri else ri.partition(":")[0]
         if any(c in path for c in string.whitespace):
             raise ValueError("Path can't have white character in it.")
-        return self.path_prefix / path
+        if path == "/":
+            return SHVPath("")
+        return self.path_prefix / path.rstrip("/")
 
     @property
     def method(self) -> str: